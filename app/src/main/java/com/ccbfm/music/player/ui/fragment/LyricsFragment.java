8c2423de69dfe679c5328ba40aa858f15ed53a3b
ccbfm <ccbfm123@163.com>
2020-03-18 17:31:24 +0800

    20200318 1731

--- app/src/main/java/com/ccbfm/music/player/ui/fragment/LyricsFragment.java
+++ app/src/main/java/com/ccbfm/music/player/ui/fragment/LyricsFragment.java
@@ -3,6 +3,10 @@ package com.ccbfm.music.player.ui.fragment;
 import com.ccbfm.music.player.R;
 import com.ccbfm.music.player.databinding.FragmentLyricsBinding;
 
+/**
+ * 歌词
+ * 示波器
+ */
 public class LyricsFragment extends BaseFragment<FragmentLyricsBinding> {
 
     @Override
--- app/src/main/java/com/ccbfm/music/player/ui/widget/ControlView.java
+++ app/src/main/java/com/ccbfm/music/player/ui/widget/ControlView.java
@@ -1,26 +1,53 @@
 package com.ccbfm.music.player.ui.widget;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
-import android.widget.FrameLayout;
+import android.view.View;
 
-public class ControlView extends FrameLayout {
+import com.ccbfm.music.player.R;
+
+public class ControlView extends View {
+
+    private Paint mBgPaint;
+    private Paint mBarPaint;
+    private Path mPlayPath;
+    private Path mPausePath;
 
     public ControlView(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public ControlView(Context context, AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public ControlView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+        this(context, attrs, defStyleAttr, 0);
     }
 
     public ControlView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        mBgPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mBgPaint.setColor(getContext().getResources().getColor(R.color.color_f2f2f2));
+        mBgPaint.setStyle(Paint.Style.FILL);
+
+        mBarPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mBarPaint.setColor(getContext().getResources().getColor(R.color.color_515151));
+        mBarPaint.setStyle(Paint.Style.FILL);
     }
 
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
 
+        canvas.drawCircle(100, 100, 100, mBgPaint);
+
+        mPlayPath.rewind();
+        mPausePath.rewind();
+
+        
+    }
 }
